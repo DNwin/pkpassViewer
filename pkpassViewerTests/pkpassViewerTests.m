e2148a68261df7d9bd181920ff9aed97c49669b0
DNwin <ndennis@gmail.com>
2015-08-10 20:48:35 -0700

    Initial Commit

---
+++ pkpassViewerTests/pkpassViewerTests.m
@@ -0,0 +1,40 @@
+//
+//  pkpassViewerTests.m
+//  pkpassViewerTests
+//
+//  Created by Dennis Nguyen on 8/10/15.
+//  Copyright (c) 2015 dnwin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface pkpassViewerTests : XCTestCase
+
+@end
+
+@implementation pkpassViewerTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
